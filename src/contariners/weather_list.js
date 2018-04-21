08517bd11db807dcc2eae7a7af58fac0c6fb67e7
Holger_Tidemand <tidemand.holger@gmail.com>
2018-04-21 20:34:21 +0200

    adds charts for pressure & humidity

--- src/contariners/weather_list.js
+++ src/contariners/weather_list.js
@@ -1,6 +1,7 @@
 import React, {Component } from 'react';
 import {connect} from 'react-redux';
-import {Sparklines, SparklinesLine} from 'react-sparklines';
+import Chart from '../components/chart';
+
 
 
 class WeatherList extends Component {
@@ -8,18 +9,20 @@ class WeatherList extends Component {
   renderWeather(cityData){
     const name = cityData.city.name
     const temp = cityData.list.map(weather => weather.main.temp);
+    const pressures = cityData.list.map(weather => weather.main.pressure);
+    const humidities = cityData.list.map(weather => weather.main.humidity);
 
     return (
       <tr key={ name }>
         <td>{ name }</td>
-        <td>
-        <Sparklines height={100} width={150} data={temp} >
-          <SparklinesLine color="blue" />
-        </Sparklines>
-        </td>
+        <td><Chart data={ temp } color="orange" /></td>
+        <td><Chart data={ pressures } color="blue" /></td>
+        <td><Chart data={ humidities } color="black" /></td>
       </tr>
     );
   }
+
+  
   render(){
     return (
       <table className="table table-hover"> 
